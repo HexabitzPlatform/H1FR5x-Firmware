7579b65610c926f78657f91a499a7b87ae829aa5
Mahmood_Reda <mr4141705@gmail.com>
2024-03-31 13:51:08 +0300

    Editing main.c  file

--- User/main.c
+++ User/main.c
@@ -13,19 +13,13 @@
 /* Private function prototypes -----------------------------------------------*/
 
 /* Main function ------------------------------------------------------------*/
-float longdegree, latdegree, speedknot, speedkm;
-char longindicator,latindicator;
-uint8_t hours, min, sec;
 
-float height;
 int main(void){
 
 	Module_Init();		//Initialize Module &  BitzOS
 
 	//Don't place your code here.
-	for(;;){
-
-		}
+	for(;;){}
 }
 
 /*-----------------------------------------------------------*/
@@ -33,15 +27,8 @@ int main(void){
 /* User Task */
 void UserTask(void *argument){
 
-
 	// put your code here, to run repeatedly.
 	while(1){
-
-		GPSHandel();
-		GetPosition(&longdegree, &latdegree, &longindicator, &latindicator);
-		GetUTC(&hours, &min, &sec);
-		GetSpeed(&speedknot, &speedkm);
-		GetHeight(&height);
 
 	}
 }
