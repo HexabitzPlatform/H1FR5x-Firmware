b3c850008aa0e8361db7ea7c20648ddf31f3b9f4
ADEL\adool <adoolfaki@gmail.com>
2024-08-04 13:14:37 +0300

    Final Release candidate ---> 3.6

--- User/main.c
+++ User/main.c
@@ -14,21 +14,22 @@
 
 /* Main function ------------------------------------------------------------*/
 
-int main(void){
+int main(void) {
 
 	Module_Init();		//Initialize Module &  BitzOS
 
 	//Don't place your code here.
-	for(;;){}
+	for (;;) {
+	}
 }
 
 /*-----------------------------------------------------------*/
 
 /* User Task */
-void UserTask(void *argument){
+void UserTask(void *argument) {
 
 	// put your code here, to run repeatedly.
-	while(1){
+	while (1) {
 
 	}
 }
